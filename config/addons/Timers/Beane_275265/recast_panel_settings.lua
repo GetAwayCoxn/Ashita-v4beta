e52e8e6a850501db463fcf951bac422de8920e0b
GetAwayCoxn <erb.daniel@outlook.com>
2022-03-15 11:47:05 -0500

    sync
    sync

--- config/addons/Timers/Beane_275265/recast_panel_settings.lua
+++ config/addons/Timers/Beane_275265/recast_panel_settings.lua
@@ -2,13 +2,13 @@ require('common');
 
 local settings = T{ };
 settings["move"] = T{ };
-settings["move"]["drag_y"] = 6;
+settings["move"]["dragging"] = false;
 settings["move"]["drag_x"] = 100;
 settings["move"]["shift_down"] = false;
-settings["move"]["dragging"] = false;
+settings["move"]["drag_y"] = 6;
 settings["scale"] = 1;
-settings["party_buffs"] = true;
 settings["padding"] = 1;
+settings["party_buffs"] = true;
 settings["locked"] = false;
 settings["opacity"] = 1;
 
