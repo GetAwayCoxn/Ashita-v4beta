da29201b67df7b9b0ee41d66710d2bf6635ba981
GetAwayCoxn <66495755+GetAwayCoxn@users.noreply.github.com>
2022-08-04 09:42:11 -0400

    sync

---
+++ config/addons/pokecity/defaults/settings.lua
@@ -0,0 +1,19 @@
+require('common');
+
+local settings = T{ };
+settings["background"] = T{ };
+settings["mobs"] = T{ };
+settings["font_height"] = 12;
+settings["font_family"] = "Arial";
+settings["color"] = 4294967295;
+settings["red"] = "|cFFFF0000|";
+settings["background"]["visible"] = true;
+settings["background"]["color"] = 4278190080;
+settings["mini"] = false;
+settings["position_x"] = 500;
+settings["green"] = "|cFF00FF00|";
+settings["yellow"] = "|cFFFFFF00|";
+settings["visible"] = true;
+settings["position_y"] = 500;
+
+return settings;
